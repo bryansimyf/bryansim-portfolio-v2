5ab832a5a91e4bdc9ac98ca0f1338f91db7c6d9b
bryansimyf <bryanyf@hotmail.com>
2026-06-23 01:55:26 +0800

    v1.0.8

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,5 +2,5 @@ import dayjs from "dayjs";
 
 export const APP_INFO = {
   version: `v${__APP_VERSION__}`,
-  lastUpdated: dayjs("2026-03-13").format("DD MMMM YYYY"),
+  lastUpdated: dayjs("2026-06-22").format("DD MMMM YYYY"),
 };
